4bf04fae31e261a355849b2b1b3c2ea6ffb1f858
dagrons <heyuehuii@126.com>
2022-05-14 11:58:45 +0800

    update setup.py add install_requires

--- setup.py
+++ setup.py
@@ -11,4 +11,20 @@ setup(
     packages = setuptools.find_packages(),
     include_package_data=True,
     package_data = {'': ['malware_classification/trained_models/*', 'malware_sim/trained_models/*']},
+    install_requires = [
+        'celery==5.1.2',
+        'gensim==4.0.1',
+        'imageio==2.18.0',
+        'mongoengine==0.23.1',
+        'numpy==1.22.3',
+        'pandas==1.4.2',
+        'pefile==2021.5.24',
+        'Pillow==9.1.0',
+        'py2neo==2021.1.5',
+        'Pygments==2.3.1',
+        'pymongo==3.11.4',
+        'redis==3.5.3',
+        'torch==1.11.0',
+        'torchsummary==1.5.1',
+    ]
 )
