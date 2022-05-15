f2d975de8ab7f35fc83e931a714bcb7df6a21692
dagrons <heyuehuii@126.com>
2022-05-15 17:26:45 +0800

    fix path not found error

--- mtools/__init__.py
+++ mtools/__init__.py
@@ -2,14 +2,19 @@ import os
 
 import gdown
 
-cls_model_path = os.path.join(os.path.dirname(__file__), "trained_models/malware_sim%doc2vec.pth")
+cls_model_folder = os.path.join(os.path.dirname(__file__), "trained_models")
+cls_model_path = os.path.join(cls_model_folder, "malware_sim%doc2vec.pth")
+if not os.path.exists(cls_model_folder):
+    os.mkdir(cls_model_folder)
 if not os.path.exists(cls_model_path):
     gdown.download(url="https://drive.google.com/file/d/1R8JwkyJIk6zE2ADFF2Lys5zNPpOlBJah/view?usp=sharing",
                    output=cls_model_path,
                    fuzzy=True)
 
-
-sim_model_path = os.path.join(os.path.dirname(__file__), 'trained_models/malware_classification%resnet34%best.pt')  # 要使用的模型字典地址
+sim_model_folder = os.path.join(os.path.dirname(__file__), 'trained_models')
+sim_model_path = os.path.join(sim_model_folder,  'malware_classification%resnet34%best.pt')  # 要使用的模型字典地址
+if not os.path.exists(sim_model_folder):
+    os.mkdir(sim_model_folder)
 if not os.path.exists(sim_model_path):
     gdown.download(url="https://drive.google.com/file/d/1zFJebJ991tzqnYWsUdcaRaaAZ377y4Wg/view?usp=sharing",
                    fuzzy=True,
